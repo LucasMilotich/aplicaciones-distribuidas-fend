a0831ba212f607eb12793655ea70710245c036f7
lucasmilotich <lucas.milotich@gmail.com>
2019-08-12 20:32:51 -0300

    view detail

--- screens/MovieDetailScreen.js
+++ screens/MovieDetailScreen.js
@@ -1,49 +1,98 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 import { Button, Text, View, StyleSheet, Image } from 'react-native';
+import { Title } from 'react-native-paper';
+import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards'
+
+
 
 export default class MovieDetailScreen extends React.Component {
-    static navigationOptions = {
-      title: 'MovieDetailScreen',
-      headerBackTitle: 'Detalles'
-    };
+  static navigationOptions = {
+    title: 'MovieDetailScreen',
+    headerBackTitle: 'Detalles'
+  };
 
-    getImage(path){
-        console.log("https://image.tmdb.org/t/p/w200" + path);
-        return "https://image.tmdb.org/t/p/w200" + path;
-    }
-  
-    render() {
-      const source = this.props.navigation.state.params.source;
-      const item = this.props.navigation.state.params.item;
-      let name = "";
-      let vote_average = "";
-      if (item != null) {
-        name = item.title;
-        image = item.poster_path;
-        vote_average = item.vote_average;
-      }
-      return (
-        <View style={styles.container}>
-          <Image source={{uri: this.getImage(image)}} style={{width: 400, height: 400}}/>
-          <Text style={styles.text}>Título: {name}</Text>
-          <Text style={styles.text}>Promedio puntaje:{vote_average}</Text>
-          <Button title="Agregar comentario" /><Button title="Ver comentarios" />
-        </View>
-      );
-    }
-    _goHome = async () => {
-      this.props.navigation.navigate('LinksScreen');
-    };
+  getImage(path) {
+    console.log("https://image.tmdb.org/t/p/w400" + path);
+    return "https://image.tmdb.org/t/p/w400" + path;
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text: {
-      fontSize: 16,
+
+  render() {
+    const source = this.props.navigation.state.params.source;
+    const item = this.props.navigation.state.params.item;
+    let name = "";
+    let vote_average = "";
+    let overview = "";
+    if (item != null) {
+      name = item.title;
+      image = item.poster_path;
+      otherImage = item.backdrop_path
+      vote_average = item.vote_average;
+      overview = item.overview
     }
-  });
+    return (
+      <View style = {{ height: '100%', backgroundColor: 'grey'}}>
+      <Card
+        style= {{
+          flexWrap: 'wrap',
+          flexGrow: '1'}}
+
+
+      >
+        
+
+       
+
+<CardImage
+          source={{ uri: this.getImage(image) }}
+          style={{   width: '100%', height: '110%',flexWrap: 'wrap'}} 
+        />
+         <CardTitle
+          style ={{marginTop: '10%'}}
+          title={name}
+          subtitle={"Puntaje promedio " + vote_average}
+        />
+        
+        <CardContent  style = {{flexWrap: 'wrap'}} text={ overview } />
+
+
+
+        
+
+
+
+
+      </Card>
+      <CardAction
+          separator={true}
+          inColumn={false}>
+          <CardButton
+            onPress={() => { }}
+            title="Agregar comentario"
+            color="blue"
+          />
+          <CardButton
+            onPress={() => { }}
+            title="Ver comentarios"
+            color="blue"
+            
+          />
+        </CardAction>
+      </View>
+    );
+  }
+  _goHome = async () => {
+    this.props.navigation.navigate('LinksScreen');
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 16,
+  }
+});
