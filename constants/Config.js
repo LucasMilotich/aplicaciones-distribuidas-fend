94594b03b5291c058c7bdc0b0dd06859d623f294
lucasmilotich <lucas.milotich@gmail.com>
2019-08-14 20:21:31 -0300

    .

--- constants/Config.js
+++ constants/Config.js
@@ -1,6 +1,6 @@
 export default {
     url: 'https://api.themoviedb.org/3/search/movie?api_key=',
-    url2: 'http://localhost:8080/',
+    url2: 'https://aplicaciones-distribuidas-bend.herokuapp.com/',
     apikey: '71a4edb01b7d0d764e48e21bec96f77a',
-    api_url: 'http://localhost:8080/'
+    api_url: 'https://aplicaciones-distribuidas-bend.herokuapp.com/'
   }
